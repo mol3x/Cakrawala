ee565be0f52a15c9f12d384e8975b54b91fdf46c
mol3x <72003120+mol3x@users.noreply.github.com>
2024-12-04 23:45:23 +0700

    Update 2024-12-03-000009_CreateUsers.php
    gak tau kenapa ganti sendiri, udah dicek taapi ganti sendiri jadi CreateSettingTable

--- app/Database/Migrations/2024-12-03-000009_CreateUsers.php
+++ app/Database/Migrations/2024-12-03-000009_CreateUsers.php
@@ -4,7 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class CreateSettingTable extends Migration
+class CreateUsersTable extends Migration
 {
     public function up()
     {
